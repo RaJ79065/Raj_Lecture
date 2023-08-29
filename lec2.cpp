6cef4e819fe63e4808b839d7bf50689fdd254819
RAJ <raju78@gmail.com>
2023-08-29 11:14:11 +0530

    cmt3

--- lec2.cpp
+++ lec2.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 
-void bubbleSort(int arr[], int size) {
-    for (int i = 0; i < size - 1; ++i) {
-        for (int j = 0; j < size - i - 1; ++j) {
-            if (arr[j] > arr[j + 1]) {
-                // Swap arr[j] and arr[j+1]
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
+void insertionSort(int arr[], int size) {
+    for (int i = 1; i < size; ++i) {
+        int key = arr[i];
+        int j = i - 1;
+
+        
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
         }
+        arr[j + 1] = key;
     }
 }
 
@@ -22,11 +23,11 @@ int main() {
         std::cout << arr[i] << " ";
     }
 
-    bubbleSort(arr, size);
+    insertionSort(arr, size);
 
     std::cout << "\nSorted array: ";
     for (int i = 0; i < size; ++i) {
         std::cout << arr[i] << " ";
     }
-return 0;
+    return 0;
 }
